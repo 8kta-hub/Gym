21258a231e401ecfb12b1f457b7dcfb841cf971b
Valentino Ibarra <ibarravalentino15@gmail.com>
2026-02-11 02:45:49 -0300

    Crear Rama DBCon

--- M/ConDB.cs
+++ M/ConDB.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Gym.M
 {
-    internal class ConDB
+    public class ConDB
     {
+        public static string strcnn = "Data Source = DESKTOP-2UJD34U\\SQLEXPRESS; Initial Catalog = gim_db; Integrated Security = True;";
+        //public static string strcnn = "Data Source = OKTY; Initial Catalog = gim_db; Integrated Security = True;";
+
+
+        public void CargarTabla(string consulta)
+        {
+            using(SqlConnection cnn = new SqlConnection(strcnn))
+            {
+                SqlCommand command = new SqlCommand(consulta, cnn);
+            }
+        }
     }
 }
